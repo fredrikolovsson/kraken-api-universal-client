15a91946d8b7be341d49c34e67ef93dca1ce345e
Fredrik Olovsson <fogithub@gmail.com>
2017-09-24 19:10:23 +0200

    Add index (main entry point)

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import KrakenClient, { createPublicKrakenClient } from './client'
+import * as constants from './constants'
+
+export default KrakenClient
+export { constants, createPublicKrakenClient }
